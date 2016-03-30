ea25369b734b353e9012bcd1067080ed4736e3db
Jole <dingyukun_2008@163.com>
2016-03-30 12:33:27 +0800

    version 1.3.0 wechar

--- wechat/index.php
+++ wechat/index.php
@@ -1,18 +1,22 @@
+<?php
 private function checkSignature()
 {
     $signature = $_GET["signature"];
     $timestamp = $_GET["timestamp"];
     $nonce = $_GET["nonce"];
 
-    $token = TOKEN;
+    $token = "DaziranFishpond";
     $tmpArr = array($token, $timestamp, $nonce);
     sort($tmpArr, SORT_STRING);
     $tmpStr = implode( $tmpArr );
     $tmpStr = sha1( $tmpStr );
 
-    if( $tmpStr == $signature ){
-    return true;
-    }else{
-    return false;
-}
+    if( $tmpStr == $signature )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
